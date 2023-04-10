2d0a81c718f848ba8d9953dc441d352f3ceb421b
imraj singh <92265647+imrajsinghsandhu@users.noreply.github.com>
2023-04-10 11:34:42 +0800

    #3 [Imraj] sending 1.6MB of data over to server
    Packet loss was not able to be seen, unless forcefully randomised packet drops are allowed

--- udp/server.py
+++ udp/server.py
@@ -1,5 +1,7 @@
 import socket
 
+bufferSize = 1
+
 '''
 a udp connection is a socketless connection
 we do not need to create a tunnel connection or perform 3 way handshake 
@@ -7,7 +9,7 @@ to create a reliable connection with a server unlike TCP/IP
 '''
 def main():
 
-    # In summary, AF_INE is used to specify the address family (IPv4 in this case
+    # In summary, AF_INET is used to specify the address family (IPv4 in this case
     # SOCK_DGRAM is used to specify the socket type (datagram socket in this case, which uses the UDP protocol)
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     localIP = "127.0.0.1"
@@ -19,21 +21,11 @@ def main():
     # keep the port hot, open to receiving messages from clients
     while True:
         
-        # refers to the maximum num of bytes to be received in a single packet
-        # max of 64kB for non ethernet networks 
-        bufferSize = 1024
-
         # recvfrom will receive data from incoming message for up to bufferSize bytes
         # excess will be truncated, so send data in packets of bufferSize bytes
-        data, address = sock.recvfrom(1024)
-
-        print("rec'd message from {}: {}".format(address, data.decode("utf-8")))
-        
-        # reply with the packet size in bytes
-        length = len(data)
-
-        # now reply to the client
-        replyMessage = "Hello friend! Thanks for packet no. {}, size : {}!".format(packetCount, length)
+        data, address = sock.recvfrom(bufferSize)
+        # now reply to the client with the count of packets
+        replyMessage = "{}".format(packetCount)
         sock.sendto(replyMessage.encode("utf-8"), address)
         packetCount += 1
 
