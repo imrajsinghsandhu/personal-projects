ce0bb605e937e142bd1d0b697491417e41b2b139
imraj singh <92265647+imrajsinghsandhu@users.noreply.github.com>
2023-04-12 11:01:47 +0800

    Update client.py
    Edit branch description in client.py

--- udp/client.py
+++ udp/client.py
@@ -1,7 +1,17 @@
 import socket
 import server
 import math
-import time
+
+'''
+in a nutshell, you just gotta send data to the UDP server, and that it. No verification is required.
+UDP is a socket connection, its just not reliable, meaning its focused on speed, and does not have 
+packet tracking or resending lost/corrupted packets
+
+Having packet tracking and resending lost/corrupted packets adds to overhead, and is more suitable for a
+service that requires a reliable connecton - such as TCP/IP
+
+UDP is used for low latency services, and is used for systems where packet loss is allowed.
+'''
 
 '''
 in this branch, we need to create a timestamp feature, that allows us to monitor the timestamps of callbacks from
@@ -11,7 +21,6 @@ server.
 3. end the timestamp when call back received
 4. print out the timestamp
 '''
-
 # initialise the socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
@@ -19,7 +28,9 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 localIP = "127.0.0.1"
 port = 8000
 
+server_bufferSize = server.bufferSize
 # send a message to the server
+
 # read txtfile
 fileContents = open("transferText.txt", "r")
 message = fileContents.read()
@@ -27,13 +38,12 @@ message_bytes = message.encode("utf-8")
 print("length of message in bytes: {}".format(len(message_bytes))) 
 
 # Split the string into fixed-size chunks
-chunks = [message_bytes[i:i+server.buffer_size] for i in range(0, len(message_bytes), server.buffer_size)]
+chunks = [message_bytes[i:i+server_bufferSize] for i in range(0, len(message_bytes), server_bufferSize)]
 num_iters_for_full_transfer = len(chunks)
 # iterate for num_iters_for_full_transfer of times to fully transfer the message
 
 packetsReceived = 0
 
-timestamp_start = time.time()
 for i in range(0, num_iters_for_full_transfer):
     # will transfer each packet
     sock.sendto(chunks[i], (localIP, port))
@@ -43,17 +53,10 @@ for i in range(0, num_iters_for_full_transfer):
     dataSize = data.decode("utf-8")
     packetsReceived = dataSize
 
-timestamp_end = time.time()
-
-diff_time = timestamp_end - timestamp_start # in seconds
-
-if ((int(packetsReceived) * server.buffer_size) < num_iters_for_full_transfer):
+if ((int(packetsReceived) * server_bufferSize) < num_iters_for_full_transfer):
     print("PACKET LOSTTTT")
 else :
     print("Alls good")
-
-# printing time taken for callback
-print("Time taken : {:.5f} seconds".format(diff_time))
 
 # # close the socket
 # sock.close()
